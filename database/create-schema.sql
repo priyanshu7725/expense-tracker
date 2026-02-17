821823b9165a1c5f9fd1e89b88d5a1e90d7354be
priyanshu7725 <priyanshukumar3424@gmail.com>
2026-02-18 00:05:20 +0530

    Initial Commit - Spring Boot expense tracker setup

---
+++ database/create-schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS expense_tracker;
+USE expense_tracker;
+
+DROP TABLE IF EXISTS expense;
+
+CREATE TABLE expense (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    date DATE NOT NULL,
+    amount DOUBLE NOT NULL,
+    category VARCHAR(255));
